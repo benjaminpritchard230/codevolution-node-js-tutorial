a5dbf08f1440bf4b35018072cc95a77e1cea6f0a
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-04-19 14:31:42 +0100

    Learn about node.js

---
+++ modules/iife.js
@@ -0,0 +1,10 @@
+(function (message) {
+  const superhero = "batman";
+  console.log(superhero);
+  console.log(message);
+})("hello");
+(function (message) {
+  const superhero = "superman";
+  console.log(superhero);
+  console.log(message);
+})("hey");
