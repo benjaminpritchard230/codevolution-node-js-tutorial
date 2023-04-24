b688c063662c18ad437fa299ce3b6aa5c72eaa2c
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-04-24 22:50:38 +0100

    Learn node.js

--- built-in-modules/http-module/index.js
+++ built-in-modules/http-module/index.js
@@ -2,11 +2,24 @@ const http = require("node:http");
 const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.createReadStream(__dirname + "/index.html").pipe(res);
-
-  //   const html = fs.readFileSync("./index.html", "utf-8");
-  //   res.end(html);
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "Text/plain" });
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "Text/plain" });
+    res.end("About page");
+  } else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        firstName: "Bruce",
+        lastName: "Wayne",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 server.listen(3000, () => {
