dab2d38faa64a6e07699d49816cfdb34e727d9b7
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-04-26 19:15:57 +0100

    Learn node.js

--- packages/my-custom-package/index.js
+++ packages/my-custom-package/index.js
@@ -1,5 +1,9 @@
+const upperCase = require("upper-case").upperCase;
+
 function greet(name) {
-  console.log(`Hello ${name} `);
+  console.log(upperCase(`Hello ${name} `));
 }
+
+greet("ben");
 
 module.exports = greet;
